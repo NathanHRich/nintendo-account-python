c1e4bc87623a8ef24c211d1b1ce8ece59f4f1c01
Nathan Rich <nathan.rich@divvypay.com>
2019-12-20 17:30:41 -0700

    pagination and error handling

--- transactions.py
+++ transactions.py
@@ -5,33 +5,37 @@ import requests
 
 def get_transactions():
     url = "https://ec.nintendo.com/api/my/transactions"
-
-    params = {'limit': '100', 'offset': '0'}
-
+    limit = '100'
+    params = {'limit': limit, 'offset': '0'}
     headers = {
         'cookie': f'JSESSIONID={os.environ["SESSION_ID"]}',
     }
-
-    response = requests.get(url, headers=headers, params=params)
-    # print(response.json())
-
-    return response.json()
+    all_transactions = []
+    while params:
+        response = requests.get(url, headers=headers, params=params).json()
+        if 'error' in response.keys():
+            print(response)
+            exit(0)
+        all_transactions += response['transactions']
+        cur_length = response['length'] + response['offset']
+        if cur_length < response['total']:
+            params = {'limit': limit, 'offset': str(cur_length)}
+        else:
+            params = None
+    return all_transactions
 
 
 if __name__ == '__main__':
-    transactions_response = get_transactions()
-    transactions = transactions_response['transactions']
+    transactions = get_transactions()
+    transactions = sorted(transactions, key=lambda i: i['date'])
+    # pprint(transactions)
     total = 0
-    # print(transactions)
-    print(f'Total games: {transactions_response["total"]}')
-    # pprint(transactions[0])
     for tran in transactions:
         if 'amount' in tran.keys():
-            # print(f"{tran['title']} {tran['amount']['formatted_value']}\t{tran['date']}")
+            print(f"\nTitle: {tran['title']}\n"
+                  f"Cost: {tran['amount']['formatted_value']}\n"
+                  f"Date: {tran['date']}")
             total += float(tran['amount']['raw_value'])
-    print(f'\nTotal Amount: ${round(total, 2)}')
-    #
-    # response1 = requests.get('https://accounts.nintendo.com/login')
-    # print(response1.text)
-
-
+    print('\n-----------------------------------------------------------------------------------\n')
+    print(f'Total Titles: {len(transactions)}')
+    print(f'Total Cost: ${round(total, round(2))}')
